1a192d2129afe7033fc9c5f8affbaf43a74d6292
noUser <noUser>
2019-07-29 20:27:45 +0300

    regex_example

---
+++ index.js
@@ -0,0 +1,25 @@
+//regex its a pattern that helps us to validate inputs
+//2 ways:
+//1=> new RegExp("PATTERN")
+//2=> /PATTERN/
+
+//to validate the input we will use test method
+
+const numbersRegex = new RegExp('^[0-9]+$')
+numbersRegex.test("sdgdsfd")// false
+numbersRegex.test("45454")// true
+
+const lpRegex = new RegExp('^[A-Z]{1,3}-[A-Z]{1,2}-[0-9]{1,4}$')
+lpRegex.test("A-A-343")// true
+lpRegex.test("A-A-454555")// false
+
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const form = document.getElementById("formValidation");
+const DOM = {
+    userName: form["userName"],
+    error: form.querySelector("#error")
+}
+
+console.log(DOM)
