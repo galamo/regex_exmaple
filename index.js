e61045aed04567f95304ecb154314086652bc018
noUser <noUser>
2019-08-01 18:47:42 +0300

    send_button

--- index.js
+++ index.js
@@ -15,42 +15,55 @@ lpRegex.test("A-A-454555"); // false
 
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const isError = { userName: true, password: true, passwordMatch: true }
 const form = document.getElementById("formValidation");
 const DOM = {
-  userName: form.querySelector("input[name=userName]"),
-  error: form.querySelector("#error"),
-  success: form.querySelector("#success"),
-  password: form.querySelector("input[name=password]"),
-  passwordMatch: form.querySelector("input[name=passwordMatch]"),
-  passwordMessage: form
-    .querySelector("#password")
-    .querySelector("#passwordMessage"),
-  passwordMatchMessage: form
-    .querySelector("#passwordMatch")
-    .querySelector("#passwordMatchMessage")
+    userName: form.querySelector("input[name=userName]"),
+    userNameMessage: form.querySelector("#userNameMessage"),
+    password: form.querySelector("input[name=password]"),
+    passwordMatch: form.querySelector("input[name=passwordMatch]"),
+    passwordMessage: form.querySelector("#passwordMessage"),
+    passwordMatchMessage: form.querySelector("#passwordMatchMessage"),
+    sendButton: form.querySelector("#send")
 };
 
 console.log(DOM);
 
-DOM.userName.addEventListener("input", function(event) {
-  resetErrors();
-  const { value } = event.currentTarget;
-  if (!value) return raiseMessage(DOM.error, "Input Is Required");
-  const emailValidationResult = validateEmail(value);
-  if (!emailValidationResult)
-    return raiseMessage(DOM.error, "Its not an email");
+DOM.userName.addEventListener("input", function (event) {
+    isError.userName = true;
+    resetErrors();
+    const { value } = event.currentTarget;
+    if (!value) return raiseMessage(DOM.userNameMessage, "Input Is Required", true);
+    const emailValidationResult = validateEmail(value);
+    if (!emailValidationResult)
+        return raiseMessage(DOM.userNameMessage, "Its not an email", true);
 
-  return raiseMessage(DOM.success, "You are ok!");
+    isError.userName = false;
+
+    return raiseMessage(DOM.userNameMessage, "V");
 });
 function resetErrors() {
-  const { error, success } = DOM;
-  error.innerHTML = "";
-  success.innerHTML = "";
+    const { userNameMessage } = DOM;
+    userNameMessage.innerHTML = "";
+
 }
 function validateEmail(input) {
-  return emailRegex.test(input.toLowerCase());
+    return emailRegex.test(input.toLowerCase());
 }
 
-function raiseMessage(element, message) {
-  element.innerHTML = message;
+function raiseMessage(element, message, isError) {
+    if (isError) {
+        element.style.color = "red"
+    } else {
+        element.style.color = "green"
+    }
+
+    element.innerHTML = message;
 }
+
+DOM.sendButton.addEventListener("click", function () {
+
+    if (isError.userName || isError.password || isError.passwordMatch) return;
+    console.log("data sent to server....")
+
+})
